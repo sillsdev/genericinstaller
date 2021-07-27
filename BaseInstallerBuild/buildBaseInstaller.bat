6eba5432fdc4a1a9c9f87e7a657a041bd2e229e0
papeh <hasso_pape@sil.org>
2021-07-27 09:46:55 -0500

    Build ProcRunner (#65)
    Build ProcRunner.exe using %msbuild% before building the installer
    
    NOT implemented: fallback if %msbuild% is unset, such as when building from Nant.

--- BaseInstallerBuild/buildBaseInstaller.bat
+++ BaseInstallerBuild/buildBaseInstaller.bat
@@ -43,8 +43,16 @@ REM Ensure WiX tools are on the PATH
 where heat >nul 2>nul
 if not %errorlevel% == 0 set PATH=%WIX%/bin;%PATH%
 
+rem TODO (Hasso) 2021.07: figure out how to do this without breaking the build:
+rem if  "%msbuild%" == "" ( set msbuild=msbuild )
+
 (
 	@echo on
+	@REM CustomActions are built by WiX magic; ProcRunner must be built normally.
+	@REM Although there are no NuGet packages to restore, the Restore target is required to generate project.assets.json
+	%msbuild% ../ProcRunner/ProcRunner.sln /p:Configuration=Release /t:Restore;Rebuild
+
+) && (
 	@REM Harvest (heat) the application and data files.
 	heat.exe dir %MASTERBUILDDIR% -cg HarvestedAppFiles -gg -scom -sreg -sfrag -srd -sw5150 -sw5151 -dr APPFOLDER -var var.MASTERBUILDDIR -t KeyPathFix.xsl -out AppHarvest.wxs
 	heat.exe dir %MASTERDATADIR% -cg HarvestedDataFiles -gg -scom -sreg -sfrag -srd -sw5150 -sw5151 -dr HARVESTDATAFOLDER -var var.MASTERDATADIR -t KeyPathFix.xsl -out DataHarvest.wxs
