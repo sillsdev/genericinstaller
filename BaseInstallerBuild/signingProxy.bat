4fed364cd504097d0fd44f0c93f1954055e7e62e
Jason Naylor <jasonleenaylor@users.noreply.github.com>
2024-03-07 11:52:27 -0800

    Refactor to allow signing to be done separately (#75)
    * Add FAILIFCANTSIGN variable to prevent error codes from
      stopping a build when signingProxy.bat is called
    * Split base build into two separate steps so signing can
      be split into a different job
    * Refactor variable setting into setVars.bat

--- BaseInstallerBuild/signingProxy.bat
+++ BaseInstallerBuild/signingProxy.bat
@@ -1,6 +1,10 @@
 @REM Subroutine to sign, if able
-@REM Usage (requires %CERTPATH% and %CERTPASS% to be set ahead of time, if needed):
+@REM Usage (requires %CERTPATH% and %CERTPASS% to be set ahead of time, if needed)
+@REM Will return exit code of 1 unless you set %FAILIFUNSIGNED% to 0
 @REM   signingProxy.bat FileToSign
+
+
+if "%FAILIFUNSIGNED%" == "" set FAILIFUNSIGNED=1
 
 where sign >nul 2>nul
 if %errorlevel%==0 (
@@ -13,5 +17,6 @@ if %errorlevel%==0 (
 	)
 	if not %errorlevel%==0 (
 		echo Unable to sign %1; skipping.
+		exit /b %FAILIFUNSIGNED%
 	)
 )
