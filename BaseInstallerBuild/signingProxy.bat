97d2aca9e8d33ec618d088d7fd48b99043967836
Jason Naylor <jasonleenaylor@users.noreply.github.com>
2024-04-23 07:43:37 -0700

    Collect filenames to be signed instead of just skipping (#76)

--- BaseInstallerBuild/signingProxy.bat
+++ BaseInstallerBuild/signingProxy.bat
@@ -1,22 +1,31 @@
 @REM Subroutine to sign, if able
 @REM Usage (requires %CERTPATH% and %CERTPASS% to be set ahead of time, if needed)
-@REM Will return exit code of 1 unless you set %FAILIFUNSIGNED% to 0
 @REM   signingProxy.bat FileToSign
+@REM If you set %FILESTOSIGNLATER% to a path the request to sign will append FileToSign
+setlocal
 
-
-if "%FAILIFUNSIGNED%" == "" set FAILIFUNSIGNED=1
-
-where sign >nul 2>nul
-if %errorlevel%==0 (
-	sign %*
+@REM Check if the FILESTOSIGNLATER environment variable is set
+if not "%FILESTOSIGNLATER%"=="" (
+	@REM Append the file name to 
+	echo %~1 >> "%FILESTOSIGNLATER%
 ) else (
-	where signtool.exe >nul 2>nul
-	if %errorlevel%==0 (
-		echo Signing with specified code signing certificate ...
-		signtool.exe sign /fd sha256 /f %CERTPATH% /p %CERTPASS% /t http://timestamp.comodoca.com/authenticode %*
-	)
-	if not %errorlevel%==0 (
-		echo Unable to sign %1; skipping.
-		exit /b %FAILIFUNSIGNED%
-	)
+    @REM Check if the "sign" command is available
+    where sign >nul 2>nul
+    if %errorlevel%==0 (
+        sign %*
+    ) else (
+        @REM Check if signtool.exe is available
+        where signtool.exe >nul 2>nul
+        if %errorlevel%==0 (
+            echo Signing with specified code signing certificate ...
+            signtool.exe sign /fd sha256 /f %CERTPATH% /p %CERTPASS% /t http://timestamp.comodoca.com/authenticode %*
+        ) else (
+		    @REM No signing tool found exit with error
+            echo Unable to sign %1; skipping. To build without signing set FILESTOSIGNLATER to a path
+			echo and this script will capture the file paths which need signing
+            exit /b 1
+        )
+    )
+	@REM If signtool.exe successfully signed this script will exit with code 0
+	@REM otherwise the exit code will be the %errorlevel% that was set by sign or signtool.exe
 )
